cc5736ee665fcfc2bdc066fb615ea2cb7d18797e
MMedeiros71 <stargatevr@gmail.com>
2022-01-31 19:33:51 -0300

    Secao 4 - Topico 40

---
+++ API/Entities/CsnivSenha.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace API.Entities
+{
+    public partial class CsnivSenha
+    {
+        public string NumCotista { get; set; }
+        public string Senha { get; set; }
+        public DateTime? DataAlt { get; set; }
+    }
+}
