f9cb994de51abdbddcd30a642063ab288bcfb530
Marcelo <stargatevr@gmail.com>
2022-02-15 16:00:05 -0300

    Secao 8 - Topico 86 - Criado o metodo de extensao datetime DateTimeExtensions.cs

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using API.Extensions;
 
 // Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
 // If you have enabled NRTs for your project, then un-comment the following line:
@@ -23,7 +24,11 @@ namespace API.Entities
         public string Interests { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
-
         public ICollection<Photo> TypePhotos { get; set; }
+
+        public int GetAge()
+        {
+            return DateOfBirth.CalculateAge();
+        }
     }
 }
