84d8b53253c9be74333404e21e8bf80c72406900
Marcelo <stargatevr@gmail.com>
2022-03-03 15:17:38 -0300

    Secao 13 - topico 160 - Alteracoes no UserDto.cs para parte Cliente.

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -47,7 +47,8 @@ namespace API.Controllers
             {
                 Username = user.Username,
                 Token = _token.GenerateToken(user),
-                KnownAs = user.KnownAs
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
             };
         }
         
@@ -98,7 +99,8 @@ namespace API.Controllers
                 Username = user.Username,
                 Token = _token.GenerateToken(user),
                 PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
-                KnownAs = user.KnownAs
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
             };
         }
 
